60defdfc34b41b5e3d9ba37f26406e0928806845
Michael Murphy <murphym18@gmail.com>
2017-07-27 16:59:42 -0700

    Add rough prototype of the program
    The program has data baked in as global variables and it just plots
    distance as a function of time.
    
    The variables px10, py10, pz10 are the initial position of ship 1.
    The variables px20, py20, pz20 are the initial position of ship 2.
    
    The variables vx1, vy1, vz1 are the velocity components of ship 1.
    The variables vx2, vy2, vz2 are the velocity components of ship 2.

---
+++ dist.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+px10 = 0
+py10 = 0
+pz10 = 0
+
+vx1 = 1
+vy1 = 0
+vz1 = 10
+
+px20 = 5
+py20 = 0
+pz20 = 0
+
+vx2 = -1
+vy2 = 0
+vz2 = 0
+
+def x1(t):
+  return px10 + vx1 * t
+def y1(t):
+  return py10 + vy1 * t
+def z1(t):
+  return pz10 + vz1 * t
+def x2(t):
+  return px20 + vx2 * t
+def y2(t):
+  return py20 + vy2 * t
+def z2(t):
+  return pz20 + vz2 * t
+
+def dist2(t):
+  dx = x1(t) - x2(t)
+  dy = y1(t) - y2(t)
+  dz = z1(t) - z2(t)
+  return dx*dx + dy*dy + dz*dz
+
+def dist(t):
+  return np.sqrt(dist2(t))
+
+
+t = np.arange(0, 5, 0.02)
+d = np.vectorize(lambda t: dist(t))(t)
+
+lines = plt.plot(t, d)
+plt.setp(lines[0], linewidth=4)
+plt.show()
