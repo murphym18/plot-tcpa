55a7227b0f74c213ebc0dadf8687c9d7623afb2c
Michael Murphy <murphym18@gmail.com>
2017-07-28 21:46:05 -0700

    WIP impl find_tcpa

--- Test.py
+++ Test.py
@@ -1,9 +1,15 @@
+import numpy as np
 import math
+
+def _make_funcs(p, v):
+    def m(i):
+        return lambda t: p[i] + t * v[i]
+    return m(0), m(1), m(2)
 
 class Test:
     def __init__(self, data):
         """
-        The parameter data is a list and the elements are floats. Here is what's inside:
+        The parameter data is a list and the elements are floats. Here is what's inside data:
         drone_A_init_pos_x
         drone_A_init_pos_y
         drone_A_init_pos_z
@@ -24,16 +30,10 @@ class Test:
         self.v1 = [float(data[i]) for i in range(3, 6)]
         self.p2 = [float(data[i]) for i in range(6, 9)]
         self.v2 = [float(data[i]) for i in range(9, 12)]
-        funcs_a = [(lambda t: self.p1[i] + self.v1[i]*t) for i in range(3)]
-        funcs_b = [(lambda t: self.p2[i] + self.v2[i]*t) for i in range(3)]
-        self.x1 = funcs_a[0]
-        self.y1 = funcs_a[1]
-        self.z1 = funcs_a[2]
-        self.x2 = funcs_b[0]
-        self.y2 = funcs_b[1]
-        self.z2 = funcs_b[2]
+        self.x1, self.y1, self.z1 = _make_funcs(self.p1, self.v1)
+        self.x2, self.y2, self.z2 = _make_funcs(self.p2, self.v2)
 
-    def dist(t):
+    def dist(self, t):
         """
         Finds the distance from drone A to drone B at time t
         """
@@ -42,10 +42,19 @@ class Test:
         dz = self.z1(t) - self.z2(t)
         return math.sqrt(dx**2 + dy**2 + dz**2)
 
-    def find_tcpa():
+    def find_tcpa(self):
         """
         Finds the time, t, where the distance from drone A to drone B is smallest
         """
-        raise NotImplementedError()
+        p1 = np.array(self.p1)
+        p2 = np.array(self.p2)
+        v1 = np.array(self.v1)
+        v2 = np.array(self.v2)
+        dp = p2 - p1
+        dv = v2 - v1
+        print(dp)
+        print(dv)
+        return -np.dot(dv, dp) / np.dot(dv, dv)
+        #raise NotImplementedError()
 
 
--- dist.py
+++ dist.py
@@ -9,38 +9,21 @@ from Test import Test
 data_file = open('data.csv', newline='')
 reader = csv.reader(data_file)
 header = next(reader)
-print(header)
+
 test1 = Test(next(reader))
-p1 = [0.0, 0.0, 0.0]
-v1 = [1.0, 0.0, 0.0]
+print("p1 =",test1.p1)
+print("p2 =",test1.p2)
+print("v1 =",test1.v1)
+print("v2 =",test1.v2)
+print("tcpa =",test1.find_tcpa())
 
-p2 = [5.0, 0.0, 0.0]
-v2 = [-1.0, 1.0, 0.0]
+t = np.arange(0, 3, 0.02)
+d = np.vectorize(test1.dist)(t)
 
-def make_funcs(p, v):
-    def make_func(i):
-        return lambda t: p[i] + t*v[i]
-    return (make_func(0), make_func(1), make_func(2))
-
-x1, y1, z1 = make_funcs(p1, v1)
-x2, y2, z2 = make_funcs(p2, v2)
-
-def dist2(t):
-  dx = x1(t) - x2(t)
-  dy = y1(t) - y2(t)
-  dz = z1(t) - z2(t)
-  return dx*dx + dy*dy + dz*dz
-
-def dist(t):
-  return np.sqrt(dist2(t))
-
-def make_test(name, dist_func, tcpa, p1, v1, p2, v2):
-    make_test_docs()
-    make_test_code()
-
-t = np.arange(0, 5, 0.02)
-d = np.vectorize(lambda t: dist(t))(t)
 
 lines = plt.plot(t, d)
 plt.setp(lines[0], linewidth=4)
 plt.savefig("test.png")
+
+
+
